93cf1432155a2a4ce4f204dd679ca5c44d02e60c
AndreaGajic <63128161+AndreaGajic@users.noreply.github.com>
2026-03-09 01:14:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-# Define your navigation
+# Define navigation
 pg = st.navigation([
     st.Page("pages/app.py", title="Career Navigator"),
     st.Page("pages/roles_list.py", title="Role Descriptions"),
 ])
+
 
 pg.run()
